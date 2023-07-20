73b241c38896bebf63f8667c2c7877443fbba1d0
Runshuo Xie <xiersh@smail.nju.edu.cn>
2023-07-20 21:57:22 +0800

    initialize

---
+++ _plugins/my.rb
@@ -0,0 +1,11 @@
+# Added to solve a bug
+# frozen_string_literal: true
+module Jekyll
+  module Drops
+    class PageDrop < Drop
+      def sort
+        @obj.data["sort"]
+      end
+    end
+  end
+end
